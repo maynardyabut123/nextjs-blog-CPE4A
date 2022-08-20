91d0acb6b27df0e62d8daab6ec96529a7cd5273c
jamir2334 <jlcatacutan.student@ua.edu.ph>
2022-08-20 09:19:30 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm <b>Jamir Catacutan</b>. I like to play computer games and mobile games. My favorite food is Asado and any type of cooked chicken. You can contact me on <Link href="https://www.facebook.com/jamir2334/">Facebook.</Link></p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
